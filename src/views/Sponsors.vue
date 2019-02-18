1946f6f90ee07971eab3b0b238112c7a2727cd3e
Jeffelini <jeffreyliu0117@gmail.com>
2019-02-18 15:50:43 -0500

    Add new content to welcome page and bottom banner

--- src/views/Sponsors.vue
+++ src/views/Sponsors.vue
@@ -2,22 +2,18 @@
   <div id="sponsors-page">
     <div id="contact">
       <h1>Sponsors</h1>
-      <hr />
+      <hr>
       <p>
         Would you like to join our family of sponsors and supporter?
-        <br />
-        <br />
-        Below is a list of our 2016-2017 sponsors and supporters. Please contact
+        <br>
+        <br>Below is a list of our 2016-2017 sponsors and supporters. Please contact
         Jordan Zhang,
-        <br />
-        Vice President of Sponsorship at
+        <br>Vice President of Sponsorship at
         <a href="url">jordan.zhang@uwenactus.com</a>
         to obtain a copy of the
-        <br />
-        sponsorship package.
-        <br />
-        <br />
-        Thank you for your support!
+        <br>sponsorship package.
+        <br>
+        <br>Thank you for your support!
       </p>
     </div>
     <div id="our-sponsors">
@@ -25,7 +21,7 @@
         alt="Accounting Endowment Fund"
         src="../assets/Accounting_Endowment_Fund.png"
         class="sponsor"
-      />
+      >
     </div>
   </div>
 </template>
@@ -48,6 +44,7 @@ hr {
 #sponsors-page {
   display: flex;
   flex-direction: column;
+  height: 100vh;
 }
 
 #our-sponsors {
